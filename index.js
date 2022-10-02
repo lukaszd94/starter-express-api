9869b2f84d3986978090b474143ad663e0df14a6
Łukasz Dudek <l.dudek@dealkeepers.com>
2022-10-03 00:41:37 +0200

    test commit 1

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+    res.send('MY_ENV_VAR' +  process.env.MY_ENV_VAR)
+});
+
+
+app.listen(process.env.PORT || 3000);
